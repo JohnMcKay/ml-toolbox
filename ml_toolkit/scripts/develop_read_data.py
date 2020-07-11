de0e1f91704089be86611b143e307e1b2e08927e
JohnMcKay <JMcKayPitt@gmail.com>
2020-07-11 18:19:27 +0000

    adding data

---
+++ ml_toolkit/scripts/develop_read_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Read Arduino CSVs
+
+Created on Fri Jul 10 15:16:52 2020
+
+@author: John McKay, JMcKayPitt@gmail.com
+
+"""
+
+# === Imports
+# System Imports
+import os
+# 3rd Party Imports
+import numpy as np
+import matplotlib.pyplot as plt
+
